82e9246bbb42cb010eeab0bade165e881dd75141
nawonhee <65769072+nawonhee@users.noreply.github.com>
2024-02-20 17:44:03 +0900

    10798 verticalRead 수정

--- 2차원 배열/verticalRead_10798.java
+++ 2차원 배열/verticalRead_10798.java
@@ -26,6 +26,47 @@
 //}
 
 
+//import java.io.BufferedReader;
+//import java.io.IOException;
+//import java.io.InputStreamReader;
+//
+//public class verticalRead_10798 {
+//	public static void main(String[] args) throws IOException {
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		String[] words = new String[5];
+//		int maxLength = 0;
+//		
+//		for(int i=0;i<5;i++) {
+//			words[i] = br.readLine();
+//			if(maxLength<words[i].length()) {				
+//				maxLength = words[i].length();
+//			}
+//		}
+//		
+//		char[][] matrix = new char[5][maxLength];
+//		
+//		for(int i=0;i<5;i++) {
+//			for(int j=0;j<maxLength;j++) {
+//				if(words[i].length()>=j+1) {
+//					matrix[i][j] = words[i].charAt(j);
+//				}
+//			}
+//		}
+//		
+//		for(int i=0;i<maxLength;i++) {
+//			for(int j=0;j<5;j++) {
+//				if(matrix[j][i]!=0) {
+//					
+//					System.out.print(matrix[j][i]);
+//				}
+//			}
+//		}
+//	}
+//}
+//
+
+
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -33,31 +74,20 @@ import java.io.InputStreamReader;
 public class verticalRead_10798 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String[] words = new String[5];
-		int maxLength = 0;
+		
+		char[][] arr = new char[5][15];
 		
 		for(int i=0;i<5;i++) {
-			words[i] = br.readLine();
-			if(maxLength<words[i].length()) {				
-				maxLength = words[i].length();
+			String word = br.readLine();
+			for(int j=0;j<word.length();j++) {
+				arr[i][j] = word.charAt(j);
 			}
 		}
 		
-		char[][] matrix = new char[5][maxLength];
-		
-		for(int i=0;i<5;i++) {
-			for(int j=0;j<maxLength;j++) {
-				if(words[i].length()>=j+1) {
-					matrix[i][j] = words[i].charAt(j);
-				}
-			}
-		}
-		
-		for(int i=0;i<maxLength;i++) {
+		for(int i=0;i<15;i++) {
 			for(int j=0;j<5;j++) {
-				if(matrix[j][i]!=0) {
-					
-					System.out.print(matrix[j][i]);
+				if(arr[j][i]!=0) {					
+					System.out.print(arr[j][i]);
 				}
 			}
 		}
